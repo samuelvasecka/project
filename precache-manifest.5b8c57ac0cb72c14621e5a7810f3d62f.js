2ebcdb40be90bca1c495862f18fe4b0c0c43a087
unknown <samuel.vasecka@gmail.com>
2020-09-21 17:12:42 +0200

    Updates

---
+++ precache-manifest.5b8c57ac0cb72c14621e5a7810f3d62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688b3708732ef749cc703c4087e75ec0",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "2becb8bdfc500ef05fc4",
+    "url": "/project/static/css/main.3bdb2bee.chunk.css"
+  },
+  {
+    "revision": "4e59b6a66077def72dd8",
+    "url": "/project/static/js/2.b03f634e.chunk.js"
+  },
+  {
+    "revision": "dfe64839b14474f5f946f85e5d2672e3",
+    "url": "/project/static/js/2.b03f634e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2becb8bdfc500ef05fc4",
+    "url": "/project/static/js/main.81dd6f04.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  }
+]);
